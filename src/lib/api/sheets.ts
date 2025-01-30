e03fa147836181a2b70d15334d75a00273c06239
Lemmeyg <Gordonlemmey@gmail.com>
2025-01-29 22:06:52 -0500

    vercel6

--- src/lib/api/sheets.ts
+++ src/lib/api/sheets.ts
@@ -1,6 +1,6 @@
 import { google } from 'googleapis';
 //import { config } from 'dotenv';
-import { resolve } from 'path';
+//import { resolve } from 'path';
 import { JWT } from 'google-auth-library';
 import type { Fixture } from './football';  // Add 'type' to import only the type
 
