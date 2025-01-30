45c82473a2c61ec2ccd375c0f67e49d6e66c74c2
Lemmeyg <Gordonlemmey@gmail.com>
2025-01-29 21:25:58 -0500

    vercel2

--- src/components/leaderboard/leaderboard-table.tsx
+++ src/components/leaderboard/leaderboard-table.tsx
@@ -18,25 +18,27 @@ export interface LeaderboardEntry {
 
 export function LeaderboardTable({ data }: { data: LeaderboardEntry[] }) {
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Rank</TableHead>
-          <TableHead>Player</TableHead>
-          <TableHead>Total</TableHead>
-          <TableHead>Gameweek</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.map((entry) => (
-          <TableRow key={entry.rank}>
-            <TableCell>{entry.rank}</TableCell>
-            <TableCell>{entry.player}</TableCell>
-            <TableCell>{entry.total}</TableCell>
-            <TableCell>{entry.gameweekTotal}</TableCell>
+    <div className="overflow-x-auto">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Rank</TableHead>
+            <TableHead>Player</TableHead>
+            <TableHead>Total</TableHead>
+            <TableHead>Gameweek</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {data.map((entry) => (
+            <TableRow key={entry.rank}>
+              <TableCell>{entry.rank}</TableCell>
+              <TableCell>{entry.player}</TableCell>
+              <TableCell>{entry.total}</TableCell>
+              <TableCell>{entry.gameweekTotal}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   )
 } 
